b14c508920509e8f17f8143e55b4210420b4e4f5
Markus Törnqvist <mjt+illyria@skyhood.com>
2012-06-27 19:52:07 +0300

    Contribute get_ and set_json to class

--- json_field/fields.py
+++ json_field/fields.py
@@ -111,6 +111,18 @@ class JSONField(models.TextField):
         defaults.update(kwargs)
         return super(JSONField, self).formfield(**defaults)
 
+    def contribute_to_class(self, cls, name):
+        self.class_name = cls
+        super(JSONField, self).contribute_to_class(cls, name)
+
+        def get_json(model_instance):
+            return self.get_db_prep_value(getattr(model_instance, self.attname, None))
+        setattr(cls, 'get_%s_json' % self.name, get_json)
+
+        def set_json(model_instance, value):
+            return setattr(model_instance, self.attname, self.to_python(value))
+        setattr(cls, 'set_%s_json' % self.name, set_json)
+
 try:
     # add support for South migrations
     from south.modelsinspector import add_introspection_rules
