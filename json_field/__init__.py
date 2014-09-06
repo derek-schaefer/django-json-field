6591477627701a9ac9bdc99f01990a499b34bbf7
Derek Schaefer <derek.schaefer@gmail.com>
2014-09-06 11:18:37 -0500

    Version bump

--- json_field/__init__.py
+++ json_field/__init__.py
@@ -1,3 +1,3 @@
 from json_field.fields import JSONField
 
-__version__ = '0.5.6'
+__version__ = '0.5.7'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name = 'django-json-field',
-    version = '0.5.6',
+    version = '0.5.7',
     description = description,
     author = 'Derek Schaefer',
     author_email = 'derek.schaefer@gmail.com',
