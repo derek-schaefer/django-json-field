a00b32ec4efa5b65ef8daba9d8ac00cc94492825
Humberto Rocha <humrochagf@gmail.com>
2018-01-12 12:30:35 -0200

    Fix deprecation warning at Django 1.8 and keep backwards compatiblitity

--- json_field/forms.py
+++ json_field/forms.py
@@ -2,7 +2,13 @@ try:
     import json
 except ImportError:  # python < 2.6
     from django.utils import simplejson as json
-from django.forms import fields, util
+
+try:
+    from django.forms.utils import ValidationError
+except ImportError:  # old django versions
+    from django.forms.util import ValidationError
+
+from django.forms import fields
 
 import datetime
 from decimal import Decimal
@@ -45,9 +51,9 @@ class JSONFormField(fields.Field):
             try:
                 value = json.dumps(eval(value, json_globals, json_locals), **self.encoder_kwargs)
             except Exception as e: # eval can throw many different errors
-                raise util.ValidationError(str(e))
+                raise ValidationError(str(e))
 
         try:
             return json.loads(value, **self.decoder_kwargs)
         except ValueError as e:
-            raise util.ValidationError(str(e))
+            raise ValidationError(str(e))
