72c18b1e62c2579566abec0d65e164226b563504
Humberto Rocha <humrochagf@gmail.com>
2018-01-12 12:50:44 -0200

    Add version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name = 'django-json-field',
-    version = '0.5.7',
+    version = '0.5.8',
     description = description,
     author = 'Derek Schaefer',
     author_email = 'derek.schaefer@gmail.com',
